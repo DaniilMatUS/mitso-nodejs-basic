765eb0cf16a4ae32d0d72284bfe0829f25215955
DaniilMatUS <icit20228@gmail.com>
2026-04-07 10:35:47 +0300

    cp/cp completed

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,23 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(__dirname, 'script.js');
+    const child = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit', 'ipc']
+    });
+    
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+    
+    child.on('error', (error) => {
+        console.error('Child process error:', error);
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
